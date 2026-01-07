7fd240a50cc33ab8c660e2dc5ea20a415708295b
Eduard <stedi.str@gmail.com>
2026-01-07 23:55:45 +0300

    v0.0.2 parsel.go add() function fix

--- parcel.go
+++ parcel.go
@@ -14,7 +14,7 @@ func NewParcelStore(db *sql.DB) ParcelStore {
 
 func (s ParcelStore) Add(p Parcel) (int, error) {
 	// реализуйте добавление строки в таблицу parcel, используйте данные из переменной p
-	res, err := s.db.Exec(`ISERT INTO parcel (client, status, address, created_at) 
+	res, err := s.db.Exec(`INSERT INTO parcel (client, status, address, created_at) 
 	VALUES (:client, :status, :address, :created_at)`,
 		sql.Named("client", p.Client),
 		sql.Named("status", p.Status),
